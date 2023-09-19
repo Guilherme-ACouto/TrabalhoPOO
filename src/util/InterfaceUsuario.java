4e94332bc71f4c1940ba107fbc5c46374382803e
Guilherme-AC <couto.guilherme@pucpr.edu.br>
2023-09-19 18:28:33 -0300

    Atualização do metodo setDesc na classe Casa

--- src/util/InterfaceUsuario.java
+++ src/util/InterfaceUsuario.java
@@ -13,19 +13,14 @@ public class InterfaceUsuario {
 
     //METODO PARA PEDIR VALOR DO IMOVEL
     public static double pedirValorImovel() {
-
         while (true) {
-
             try {
-
                 System.out.print("Digite o valor do imóvel: ");
                 double valor = scanner.nextDouble();
-
                 if (valor <= 0) {
                     throw new Exception("O valor deve ser maior que zero.");
                 }
                 return valor;
-
             } catch (InputMismatchException e) {
                 System.out.println("\nValor inválido. Certifique-se de inserir um valor válido.\n");
                 scanner.nextLine();
@@ -35,21 +30,17 @@ public class InterfaceUsuario {
             }
         }
     }
-
     //METODO PARA PEDIR PRAZO DE FINANCIAMENTO
     public static int pedirPrazoFinanciamento() {
         int prazoFinanciamento;
-
         while (true)
             try {
                 System.out.print("Em quantos anos quer financiar? ");
                 prazoFinanciamento = scanner.nextInt();
-
                 if (prazoFinanciamento <= 0 || prazoFinanciamento >= 35) {
                     throw new Exception("Prazo de financiamento não aceito.");
                 }
                 return prazoFinanciamento;
-
             } catch (InputMismatchException e) {
                 System.out.println("\nValor inválido. Certifique-se de inserir um valor válido.\n");
                 scanner.nextLine();
@@ -58,23 +49,17 @@ public class InterfaceUsuario {
                 scanner.nextLine();
             }
     }
-
     //METODO PARA PEDIR TAXA DE JUROS
     public static double pedirTaxaJuros() {
         double taxaJuros;
-
         while (true) {
-
             try {
-
                 System.out.print("Digite o valor da taxa de juros: ");
                 taxaJuros = scanner.nextDouble();
-
                 if (taxaJuros <= 0) {
                     throw new Exception("O valor deve ser maior que zero.");
                 }
                 return taxaJuros;
-
             } catch (InputMismatchException e) {
                 System.out.println("\nValor inválido. Certifique-se de inserir um valor válido.\n");
                 scanner.nextLine();
